589be62b57974e3d932df0d82828eda1ef5b80e3
linabreu <linabreu@outlook.com>
2023-06-13 14:07:57 -0700

    adding the first set of problem instructions

---
+++ week04CodingAssignment.js
@@ -0,0 +1,8 @@
+/*
+Create an array called ages that contains the following values: 3, 9, 23, 64, 2, 8, 28, 93.
+Programmatically subtract the value of the first element in the array from the value in the last element of the array.
+Do not use numbers to reference the last element, find it programmatically.
+ages[7] - ages[0] is not allowed!
+Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
+Use a loop to iterate through the array and calculate the average age.
+*/
